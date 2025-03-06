e293241dd0b45cd3c1995fbaaecdabbd142f1cb6
letram055 <tramb2105564@student.ctu.edu.vn>
2025-03-07 01:03:15 +0700

    cai dat tong quan

---
+++ lib/ui/staff_barista/bottombar.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class BottomBar extends StatelessWidget {
+  final int selectedIndex;
+  final Function(int) onItemTapped;
+
+  const BottomBar({
+    required this.selectedIndex,
+    required this.onItemTapped,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Icon(Icons.list_rounded),
+          label: 'Đơn hàng',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.fastfood_rounded),
+          label: 'Danh sách món',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.list),
+          label: 'Nguyên liệu',
+        ),
+      ],
+      currentIndex: selectedIndex,
+      selectedItemColor: Theme.of(context).colorScheme.primary,
+      unselectedItemColor: Colors.black,
+      onTap: onItemTapped,
+      showUnselectedLabels: true,
+      showSelectedLabels: true,
+      type: BottomNavigationBarType.fixed,
+    );
+  }
+}
---
+++ lib/ui/staff_barista/screens.dart
@@ -0,0 +1,8 @@
+export 'bottombar.dart';
+
+export '../../managers/staff_barista/menu_manager.dart';
+export '../../managers/staff_barista/ingredient_manager.dart';
+
+export 'menu/menu_screen.dart';
+export 'order/order_screen.dart';
+export 'ingredient/ingredient_screen.dart';
